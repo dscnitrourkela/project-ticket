34fa90bb49f58dcf858b533e01878ecbc8160e6c
Srishty M <srishtymangutte@gmail.com>
2023-12-13 21:56:52 +0530

    chore: firebase setup

---
+++ src/firebase/firebase.js
@@ -0,0 +1,12 @@
+// Import the functions you need from the SDKs you need
+//import { initializeApp } from 'firebase/app'
+import 'firebase/auth'
+import 'firebase/database'
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+//import firebaseConfig from '../../.env'
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+
+// Initialize Firebase
+//const app = initializeApp(firebaseConfig)
