59908aeea53bf5ed4bf6f4a58e8baea0d10fde23
ashutosh-rath02 <ashutosh123rath@gmail.com>
2023-12-26 17:08:46 +0530

    feat: add ticket flow

---
+++ src/app/components/modal.jsx
@@ -0,0 +1,46 @@
+// components/Modal.jsx
+import React from 'react'
+import styled from 'styled-components'
+
+const StyledModal = styled.div`
+  position: fixed;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+  background-color: rgba(0, 0, 0, 0.5);
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`
+
+const ModalContent = styled.div`
+  background-color: white;
+  padding: 20px;
+  border-radius: 10px;
+  position: relative;
+`
+
+const CloseButton = styled.button`
+  position: absolute;
+  top: 10px;
+  right: 10px;
+  cursor: pointer;
+`
+
+const Modal = ({ show, onClose, children }) => {
+  if (!show) {
+    return null
+  }
+
+  return (
+    <StyledModal>
+      <ModalContent>
+        <CloseButton onClick={onClose}>X</CloseButton>
+        {children}
+      </ModalContent>
+    </StyledModal>
+  )
+}
+
+export default Modal
