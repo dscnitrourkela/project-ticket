9839c29eaf07a322a5211731fd9af0ffeace49de
anmol5936 <mangarajanmol666@gmail.com>
2024-01-09 21:18:31 +0530

    css changes

--- src/app/components/SignUp.jsx
+++ src/app/components/SignUp.jsx
@@ -16,16 +16,14 @@ const SignUp = () => {
   const [name, setName] = useState('')
   const [email, setEmail] = useState('')
   const [password, setPassword] = useState('')
+  const [mode, setMode] = useState('signup')
   const router = useRouter()
 
   const handleSignUp = async () => {
     try {
       await signUpWithEmailAndPassword(email, password, name)
       router.push('/myticket')
-      //window.alert('User signed up successfully:', user)
-    } catch (error) {
-      //window.alert('Signup error:', error.message)
-    }
+    } catch (error) {}
   }
   const handleGoogleSignup = async () => {
     try {
@@ -47,38 +45,237 @@ const SignUp = () => {
   const handlePasswordChange = (event) => {
     setPassword(event.target.value)
   }
+  const toggleMode = () => {
+    setMode(mode === 'signup' ? 'signin' : 'signup')
+  }
 
   return (
     <div className="signContainer">
+      <div className="dot top-left-dot">
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          width="14"
+          height="13"
+          viewBox="0 0 14 13"
+          fill="none"
+        >
+          <g filter="url(#filter0_d_24_318)">
+            <circle cx="6.82388" cy="6.70889" r="3.8168" fill="#CD4FF9" />
+          </g>
+          <defs>
+            <filter
+              id="filter0_d_24_318"
+              x="0.539913"
+              y="0.424922"
+              width="12.5679"
+              height="12.5679"
+              filterUnits="userSpaceOnUse"
+              color-interpolation-filters="sRGB"
+            >
+              <feFlood flood-opacity="0" result="BackgroundImageFix" />
+              <feFlood flood-opacity="0" result="BackgroundImageFix" />
+              <feColorMatrix
+                in="SourceAlpha"
+                type="matrix"
+                values="0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 127 0"
+                result="hardAlpha"
+              />
+              <feOffset />
+              <feGaussianBlur stdDeviation="1.23358" />
+              <feComposite in2="hardAlpha" operator="out" />
+              <feColorMatrix
+                type="matrix"
+                values="0 0 0 0 0.88235 0 0 0 0 0.5475 0 0 0 0 1 0 0 0 1 0"
+              />
+              <feBlend mode="normal" in2="BackgroundImageFix" result="effect1_dropShadow_24_318" />
+              <feBlend
+                mode="normal"
+                in="SourceGraphic"
+                in2="effect1_dropShadow_24_318"
+                result="shape"
+              />
+            </filter>
+          </defs>
+        </svg>
+      </div>
+      <div className="dot top-right-dot">
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          width="14"
+          height="13"
+          viewBox="0 0 14 13"
+          fill="none"
+        >
+          <g filter="url(#filter0_d_24_318)">
+            <circle cx="6.82388" cy="6.70889" r="3.8168" fill="#CD4FF9" />
+          </g>
+          <defs>
+            <filter
+              id="filter0_d_24_318"
+              x="0.539913"
+              y="0.424922"
+              width="12.5679"
+              height="12.5679"
+              filterUnits="userSpaceOnUse"
+              color-interpolation-filters="sRGB"
+            >
+              <feFlood flood-opacity="0" result="BackgroundImageFix" />
+              <feColorMatrix
+                in="SourceAlpha"
+                type="matrix"
+                values="0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 127 0"
+                result="hardAlpha"
+              />
+              <feOffset />
+              <feGaussianBlur stdDeviation="1.23358" />
+              <feComposite in2="hardAlpha" operator="out" />
+              <feColorMatrix
+                type="matrix"
+                values="0 0 0 0 0.88235 0 0 0 0 0.5475 0 0 0 0 1 0 0 0 1 0"
+              />
+              <feBlend mode="normal" in2="BackgroundImageFix" result="effect1_dropShadow_24_318" />
+              <feBlend
+                mode="normal"
+                in="SourceGraphic"
+                in2="effect1_dropShadow_24_318"
+                result="shape"
+              />
+            </filter>
+          </defs>
+        </svg>
+      </div>
+      <div className="dot bottom-left-dot">
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          width="14"
+          height="13"
+          viewBox="0 0 14 13"
+          fill="none"
+        >
+          <g filter="url(#filter0_d_24_318)">
+            <circle cx="6.82388" cy="6.70889" r="3.8168" fill="#CD4FF9" />
+          </g>
+          <defs>
+            <filter
+              id="filter0_d_24_318"
+              x="0.539913"
+              y="0.424922"
+              width="12.5679"
+              height="12.5679"
+              filterUnits="userSpaceOnUse"
+              color-interpolation-filters="sRGB"
+            >
+              <feFlood flood-opacity="0" result="BackgroundImageFix" />
+              <feColorMatrix
+                in="SourceAlpha"
+                type="matrix"
+                values="0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 127 0"
+                result="hardAlpha"
+              />
+              <feOffset />
+              <feGaussianBlur stdDeviation="1.23358" />
+              <feComposite in2="hardAlpha" operator="out" />
+              <feColorMatrix
+                type="matrix"
+                values="0 0 0 0 0.88235 0 0 0 0 0.5475 0 0 0 0 1 0 0 0 1 0"
+              />
+              <feBlend mode="normal" in2="BackgroundImageFix" result="effect1_dropShadow_24_318" />
+              <feBlend
+                mode="normal"
+                in="SourceGraphic"
+                in2="effect1_dropShadow_24_318"
+                result="shape"
+              />
+            </filter>
+          </defs>
+        </svg>
+      </div>
+      <div className="dot bottom-right-dot">
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          width="14"
+          height="13"
+          viewBox="0 0 14 13"
+          fill="none"
+        >
+          <g filter="url(#filter0_d_24_318)">
+            <circle cx="6.82388" cy="6.70889" r="3.8168" fill="#CD4FF9" />
+          </g>
+          <defs>
+            <filter
+              id="filter0_d_24_318"
+              x="0.539913"
+              y="0.424922"
+              width="12.5679"
+              height="12.5679"
+              filterUnits="userSpaceOnUse"
+              color-interpolation-filters="sRGB"
+            >
+              <feFlood flood-opacity="0" result="BackgroundImageFix" />
+              <feColorMatrix
+                in="SourceAlpha"
+                type="matrix"
+                values="0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 127 0"
+                result="hardAlpha"
+              />
+              <feOffset />
+              <feGaussianBlur stdDeviation="1.23358" />
+              <feComposite in2="hardAlpha" operator="out" />
+              <feColorMatrix
+                type="matrix"
+                values="0 0 0 0 0.88235 0 0 0 0 0.5475 0 0 0 0 1 0 0 0 1 0"
+              />
+              <feBlend mode="normal" in2="BackgroundImageFix" result="effect1_dropShadow_24_318" />
+              <feBlend
+                mode="normal"
+                in="SourceGraphic"
+                in2="effect1_dropShadow_24_318"
+                result="shape"
+              />
+            </filter>
+          </defs>
+        </svg>
+      </div>
       <div className="sign-form">
-        <div className="switch-btn">
-          <div className="signup">Signup</div>&nbsp;
-          <div className="signin">Signin</div>
+        <div className="switch-btn" onClick={toggleMode}>
+          <div className={mode === 'signup' ? 'signup active' : 'signup'}>Signup</div>&nbsp;
+          <div className={mode === 'signin' ? 'signin active' : 'signin'}>Signin</div>
         </div>
-        <p className="form-header">Your Name</p>
-        <input type="text" placeholder="Enter your name" value={name} onChange={handleNameChange} />
-        <div className="form-header">Email</div>
-        <input
-          type="email"
-          placeholder="Enter your email"
-          value={email}
-          onChange={handleEmailChange}
-        />
-        <p className="form-header">Password</p>
-        <input
-          type="password"
-          placeholder="Enter your password"
-          value={password}
-          onChange={handlePasswordChange}
-        />
+        <div className="form">
+          {mode === 'signup' && (
+            <>
+              <p className="NText">Your Name</p>
+              <input
+                type="text"
+                placeholder="Enter your name"
+                value={name}
+                onChange={handleNameChange}
+              />
+            </>
+          )}
+          <div className="NText">Email</div>
+          <input
+            type="email"
+            placeholder="Enter your email"
+            value={email}
+            onChange={handleEmailChange}
+          />
+          <p className="NText">Password</p>
+          <input
+            type="password"
+            placeholder="Enter your password"
+            value={password}
+            onChange={handlePasswordChange}
+          />
+        </div>
         <button className="signupbutton" type="submit" onClick={handleSignUp}>
-          <p className="form-header">Sign up</p>
+          <p className="SText">Sign up</p>
         </button>
         <button className="extsign" type="submit" onClick={handleGoogleSignup}>
-          Sign up with Google
+          <div className="GText"> Sign up with Google</div>
         </button>
         <button className="extsign" type="submit" onClick={signUpWithGitHub}>
-          Sign up with Github
+          <div className="GText">Sign up with Github</div>
         </button>
         <div className="footer">
           Already have an account ? &nbsp; <span> Sign in </span>
