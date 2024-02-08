858f726eb2b2c71a96efdeb10a091ee68241ca11
ishita-1305 <ishsri00@gmail.com>
2024-02-08 10:09:44 +0530

    fix:errors

--- src/app/Homepage.jsx
+++ src/app/Homepage.jsx
@@ -1,8 +1,8 @@
 import React from 'react'
 
+import { Navbar } from './components/marginals/Navbar'
 //import CreateTicket from './components/CreateTicket'
-import SignUp from './components/centres/SignUp.jsx'
-import { Navbar } from './components/marginals/navbar.jsx'
+import SignUp from './components/SignUp'
 import { AuthProvider } from './context/AuthContext'
 
 //import Link from 'next/link'
